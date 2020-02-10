35dd342125160e213657525603500b5f877e0ba2
Blair Desjardins <blairmichaeldesjardins@gmail.com>
2020-02-09 19:35:17 -0500

    add graphics for enemy

--- SpriteKitGame/SpriteKitGame/Enemy.swift
+++ SpriteKitGame/SpriteKitGame/Enemy.swift
@@ -12,10 +12,14 @@ import SpriteKit
 class Enemy: GameObject
 {
     var isDestroyed: Bool = false
-    // constructor
+    
+    private var walkFrames: [SKTexture] = []
+    
     override init()
     {
-        super.init(imageString: "enemy", size: CGSize(width: 50.0, height: 130.0))
+        super.init(imageString: "enemy", size: CGSize(width: 80.0, height: 60.0))
+        buildAnimations()
+        animate()
         Start()
     }
     
@@ -23,6 +27,25 @@ class Enemy: GameObject
         fatalError("init(coder:) has not been implemented")
     }
     
+    func buildAnimations() {
+        let walkAnimatedAtlas = SKTextureAtlas(named: "crab-walk")
+        
+        let numWalkImages = walkAnimatedAtlas.textureNames.count
+        for i in 1...numWalkImages {
+            let walkTextureName = "crab-walk-\(i)"
+            walkFrames.append(walkAnimatedAtlas.textureNamed(walkTextureName))
+        }
+    }
+    
+    func animate() {
+        self.run(SKAction.repeatForever(
+          SKAction.animate(with: walkFrames,
+                           timePerFrame: 0.1,
+                           resize: false,
+                           restore: true)),
+                 withKey:"walking")
+    }
+        
     override func CheckBounds()
     {
     }
@@ -36,7 +59,7 @@ class Enemy: GameObject
     {
         self.zPosition = 3
         
-        self.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 10, height: self.size.height))
+        self.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 10, height: 10))
         self.physicsBody?.isDynamic = true
         self.physicsBody?.affectedByGravity = false
         self.physicsBody?.categoryBitMask = CollisionCategories.Enemy
@@ -46,7 +69,7 @@ class Enemy: GameObject
     
     override func Update()
     {
-        self.position.x -= 3
+        self.position.x -= 5
     }
     
     
